bc25828fca053559fc3c778065624c5e650168ab
Ron Wellman <ron.wellman01@gmail.com>
2025-02-08 23:40:44 -0500

    Renaming of main class.

--- src/main/java/csi5347/inventory-configuration/InventoryConfigurationApplication.java
+++ src/main/java/csi5347/inventory-configuration/InventoryConfigurationApplication.java
@@ -1,4 +1,4 @@
-package csi5347.configserver;
+package csi5347.inventory-configuration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,9 +6,9 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class ConfigServerApplication {
+public class InventoryConfigurationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
+		SpringApplication.run(InventoryConfigurationApplication.class, args);
 	}
 }
